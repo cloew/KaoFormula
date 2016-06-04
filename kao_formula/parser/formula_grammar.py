4fc56e147d578ba7c4dd23568f933af7fb2a0121
Chris Loew <cloew123@gmail.com>
2016-06-04 08:35:11 -0600

    Added number pattern

--- kao_formula/parser/formula_grammar.py
+++ kao_formula/parser/formula_grammar.py
@@ -10,5 +10,6 @@ class FormulaGrammar(Grammar):
     """ Grammar to use when parsing a Formula """
     integer             = r"\d+", IntegerConverter
     decimal             = r"\d+\.\d+", DecimalConverter
+    number              = r"<decimal>|<integer>"
     identifier          = r"[a-zA-Z]+\w*", IdentifierConverter
-    formula             = r"<identifier>|<decimal>|<integer>", Formula
+    formula             = r"<identifier>|<number>", Formula
